119b0bfb10e6e8023a6004aa8ed4842025614d60
louistru <louistru@hotmail.com>
2021-01-28 18:07:34 +0800

    update btc api

--- btc.js
+++ btc.js
@@ -1,6 +1,6 @@
 
 // var somes = require('somes').default;
-var crypto = require('./account');
+var account = require('./account');
 var hash_js = require('hash.js');
 var buffer = require('somes/buffer').default;
 
@@ -12,8 +12,8 @@ function ripemd160(k, enc) {
 	return new hash_js.ripemd160().update(k).digest(enc);
 }
 
-function wif(k, compress = false, test = false) {
-	var k = buffer.concat([[test ? 0xEF : 0x80/*0x80 mainnet| 0xEF tstnet*/], k, compress ? [0x01]: []]);
+function getWIFKey(privateKey_bin, compress = false, test = false) { // get btc wif private key from private ey
+	var k = buffer.concat([[test ? 0xEF : 0x80/*0x80 mainnet| 0xEF tstnet*/], privateKey_bin, compress ? [0x01]: []]);
 	var hash = sha256(sha256(k));
 	var check = hash.slice(0, 4);
 	var wif = buffer.concat([k, check]);
@@ -21,12 +21,12 @@ function wif(k, compress = false, test = false) {
 	return wif;
 }
 
-function address(k, compress = false, test = false) {
+function getAddress(publicKey_bin, compress = false, test = false) { // get btc address from public key
 	// 0	00	P2PKH address	1	17VZNX1SN5NtKa8UQFxwQbFeFc3iqRYhem
 	// 5	05	P2SH address	3	3EktnHQD7RiAE6uzMj2ZifT9YgRrkSgzQX
 	// 111	6F	Testnet P2PKH address pub key	m or n	mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn
 	// 196	C4	Testnet P2SH address pub key	m or n	mipcBbFg9gMiCh81Kj8tqqdgoZub1ZJRfn
-	var pub = crypto.getPublic(k, compress);
+	var pub = account.publicKeyConvert(publicKey_bin, compress);
 	var hash = buffer.concat([[test ? 0x6F : 0x00], ripemd160(sha256(pub))]);
 	var check = sha256(sha256(hash)).slice(0, 4);
 	var address = buffer.concat([hash, check]);
@@ -35,14 +35,19 @@ function address(k, compress = false, test = false) {
 	return address;
 }
 
-function parseWIF(wif_bytes) {
-	var _wif = wif_bytes; // buffer.from(wif_b58, 'base58');
-	var compress = _wif.length == 38 && _wif[33] == 0x01;
-	var network = _wif[0];
-	var private = _wif.slice(1, 33);
-	var check = compress ? _wif.slice(34, 38): _wif.slice(33, 37);
-	var public = buffer.from(crypto.getPublic(private, compress));
-	var _address = address(private, compress, network == 0xEF);
+function getAddressFromPrivateKey(private_key_bin, compress = false, test = false) {
+	var public = buffer.from(account.getPublic(private_key_bin, compress));
+	return getAddress(public, compress, trse);
+}
+
+function parseWIFKey(wif_bin) { // wif key bin
+	var wif = wif_bin;
+	var compress = wif.length == 38 && wif[33] == 0x01;
+	var network = wif[0];
+	var private = wif.slice(1, 33);
+	var check = compress ? wif.slice(34, 38): wif.slice(33, 37);
+	var public = buffer.from(account.getPublic(private, compress));
+	var address = getAddress(public, compress, network == 0xEF);
 	// somes.assert(wif(private, compress).toString('base58') == wif_b58, 'parseWIF(), check code error');
 	return {
 		mainnet: network == 0x80,  /*0x80 mainnet| 0xEF tstnet*/
@@ -50,12 +55,12 @@ function parseWIF(wif_bytes) {
 		compress: compress,
 		check: check,
 		public: public,
-		address: _address,
+		address: address,
 	};
 }
 
-function parseAddress(address_bytes) {
-	var address = address_bytes; // buffer.from(address_b58, 'base58');
+function parseAddress(address_bin) {
+	var address = address_bin;
 	var type = address[0];
 	var hash = address.slice(1, 20);
 	var check = address.slice(21, 4);
@@ -65,7 +70,18 @@ function parseAddress(address_bytes) {
 	};
 }
 
-exports.wif = wif;
+function parseWIFKeyFromB58String(wif_b58_str) { // wif key base58 string
+	return parseWIFKey(buffer.from(wif_b58_str, 'base58'));
+}
+
+function parseAddressFromB58String(address_b58_str) {
+	return parseAddress(buffer.from(address_b58_str, 'base58'));
+}
+
+exports.getAddressFromPrivateKey = getAddressFromPrivateKey;
+exports.getWIFKey = getWIFKey;
+exports.parseWIFKeyFromB58String = parseWIFKeyFromB58String;
 exports.address = address;
-exports.parseWIF = parseWIF;
+exports.parseWIFKey = parseWIFKey;
 exports.parseAddress = parseAddress;
+exports.parseAddressFromB58String = parseAddressFromB58String;
